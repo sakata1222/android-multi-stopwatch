dbc074e3662529d1adf684d74e6ed9d589981e0d
hiro <sakata1222@gmail.com>
2019-02-11 19:11:19 +0900

    feat: add project template generated by android studio.

---
+++ app/src/main/java/saka/java_conf/gr/jp/multistopwatch/MainActivity.kt
@@ -0,0 +1,12 @@
+package saka.java_conf.gr.jp.multistopwatch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
